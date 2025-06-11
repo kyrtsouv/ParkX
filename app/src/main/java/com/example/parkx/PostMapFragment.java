cd4da41ff7e56cc6fca8e1fe0bd12a8bc47d3122
Panagiotis <panzach03@gmail.com>
2025-06-11 23:39:36 +0300

    add timePicker (Date+Time)

--- app/src/main/java/com/example/parkx/PostMapFragment.java
+++ app/src/main/java/com/example/parkx/PostMapFragment.java
@@ -1,7 +1,10 @@
 package com.example.parkx;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +25,8 @@ import java.time.LocalDateTime;
 
 public class PostMapFragment extends MapFragment {
 
+    private int minutes=0,hours=0,day=0,month=0,year=0;
+    private LocalDateTime localDateTime=LocalDateTime.now();
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -53,6 +58,10 @@ public class PostMapFragment extends MapFragment {
 
         TextView title = view.findViewById(R.id.textView_MAP);
         Button actionButton = view.findViewById(R.id.button_MAP);
+        Button timeButton = view.findViewById(R.id.button_Time);
+        timeButton.setOnClickListener(view1->{
+            setTime(view1);
+        });
 
         title.setText(marker.getTitle());
         actionButton.setText("Προσθήκη Θέσης Πάρκινγκ");
@@ -60,7 +69,15 @@ public class PostMapFragment extends MapFragment {
             bottomDialog.dismiss();
             LatLng temp = marker.getPosition();
 
-            SupabaseManager.publishSpot(temp.latitude, temp.longitude, LocalDateTime.now(), new JavaResultCallback<>() {
+            /// ελεγχος αν εχουν μπει τιμες τοτε ορισε ημερομηνιες αλλιως συνεχισε με την τοπική ώρα
+            if(year==0)
+                localDateTime=LocalDateTime.now();
+            else
+                localDateTime = LocalDateTime.of(year,month,day,hours,minutes);
+            Toast.makeText(getContext(), "Ημερομηνια : "+year+"-"+month+"-"+day+" "
+                    +(hours-3)+":"+minutes, Toast.LENGTH_SHORT).show();
+
+            SupabaseManager.publishSpot(temp.latitude, temp.longitude, localDateTime, new JavaResultCallback<>() {
                 @Override
                 public void onSuccess(String value) {
                     Toast.makeText(getContext(), "Σωστή Εκτέλεση", Toast.LENGTH_SHORT).show();
@@ -75,5 +92,28 @@ public class PostMapFragment extends MapFragment {
         });
         bottomDialog.setContentView(view);
         bottomDialog.show();
+    }
+
+    /// εμφανιση TimePicker για ημερομηνία και ώρα
+    private void setTime(View view){
+
+        /// προσθετω +3 στην ωρα για το 24ωρο ρολοι
+        TimePickerDialog.OnTimeSetListener onTimeSetListener= (timePicker, selectedHour, selectedMinute) -> {
+            hours=selectedHour+3;
+            minutes=selectedMinute;
+            Log.d("mytag", "Ώρα: " + selectedHour + ":" + selectedMinute);
+        };
+
+        TimePickerDialog timePickerDialog=new TimePickerDialog(getContext(),onTimeSetListener,hours,minutes,true);
+        timePickerDialog.show();
+
+        /// προσθετω +1 στο μηνα γιατι ξεκιναει απο το 0 (Ιανουαριος=0)
+        DatePickerDialog onDateSetListener =new DatePickerDialog(requireContext(), (datePicker, Year, Month, dayOfMonth) -> {
+            year=Year;
+            month=Month+1;
+            day=dayOfMonth;
+        }, LocalDateTime.now().getYear(), LocalDateTime.now().getMonthValue(), LocalDateTime.now().getDayOfMonth());
+        onDateSetListener.show();
+
     }
 }
--- app/src/main/java/com/example/parkx/SearchMapFragment.java
+++ app/src/main/java/com/example/parkx/SearchMapFragment.java
@@ -1,10 +1,14 @@
 package com.example.parkx;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,13 +26,19 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class SearchMapFragment extends MapFragment {
     private final List<Marker> markers_P = new ArrayList<>();
     private Circle circle = null;
+    private int minutes=0,hours=0,day=0,month=0,year=0;
+    private LocalDateTime localDateTime;
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
@@ -55,11 +65,21 @@ public class SearchMapFragment extends MapFragment {
     private void checkParking(Marker marker) {
         LatLng temp = marker.getPosition();
 
-        SupabaseManager.getSpots(temp.latitude, temp.longitude, LocalDateTime.now(), new JavaResultCallback<>() {
+        /// ελεγχος αν εχουν μπει τιμες τοτε ορισε ημερομηνιες αλλιως συνεχισε με την τοπική ώρα
+        if(year==0)
+            localDateTime=LocalDateTime.now();
+        else
+            localDateTime = LocalDateTime.of(year,month,day,hours,minutes);
+        Toast.makeText(getContext(), "Ημερομηνια : "+year+"-"+month+"-"+day+" "
+                +hours+":"+minutes, Toast.LENGTH_SHORT).show();
+
+        SupabaseManager.getSpots(temp.latitude, temp.longitude, localDateTime, new JavaResultCallback<>() {
             @Override
             public void onSuccess(List<ParkingSpot> value) {
                 //ακτινα κυκλου γύρο από το χρηστη
                 int RADIUS = 1000;
+                if (circle != null)
+                    circle.remove();
                 circle = mMap.addCircle(new CircleOptions().center(temp).radius(RADIUS)
                         .strokeColor(Color.CYAN).fillColor(0x220000FF).strokeWidth(3));
                 for (ParkingSpot p : value) {
@@ -107,6 +127,11 @@ public class SearchMapFragment extends MapFragment {
 
         TextView title = view.findViewById(R.id.textView_MAP);
         Button actionButton = view.findViewById(R.id.button_MAP);
+        Button timeButton = view.findViewById(R.id.button_Time);
+        timeButton.setOnClickListener(view1->{
+            setTime(view1);
+
+        });
 
         title.setText(marker.getTitle());
         actionButton.setText("Αναζήτηση Θέσης Πάρκινγκ");
@@ -137,4 +162,28 @@ public class SearchMapFragment extends MapFragment {
         bottomDialog.setContentView(view);
         bottomDialog.show();
     }
+
+    /// εμφανιση TimePicker για ημερομηνία και ώρα
+    private void setTime(View view){
+
+        /// προσθετω +3 στην ωρα για το 24ωρο ρολοι
+        TimePickerDialog.OnTimeSetListener onTimeSetListener= (timePicker, selectedHour, selectedMinute) -> {
+            hours=selectedHour+3;
+            minutes=selectedMinute;
+            Log.d("mytag", "Ώρα: " + selectedHour + ":" + selectedMinute);
+        };
+
+        TimePickerDialog timePickerDialog=new TimePickerDialog(getContext(),onTimeSetListener,hours,minutes,true);
+        timePickerDialog.show();
+
+        /// προσθετω +1 στο μηνα γιατι ξεκιναει απο το 0 (Ιανουαριος=0)
+        DatePickerDialog onDateSetListener =new DatePickerDialog(requireContext(), (datePicker, Year, Month, dayOfMonth) -> {
+            year=Year;
+            month=Month+1;
+            day=dayOfMonth;
+            }, LocalDateTime.now().getYear(), LocalDateTime.now().getMonthValue(), LocalDateTime.now().getDayOfMonth());
+        onDateSetListener.show();
+
+    }
+
 }
